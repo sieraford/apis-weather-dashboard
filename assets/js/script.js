21ef2d37f67ed0028508c22f22b24e3fe9da6514
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-11-19 19:52:40 -0800

    add search history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,43 @@ var searchField = document.getElementById("search-field")
 var searchButton = document.getElementById("search-btn")
 var cityNameEl = document.getElementById("city-name")
 var currentContainer = document.getElementById("current-weather")
+var searchHistoryContainer = document.getElementById("search-history")
 
 var APIKey = "f31f098d0ca4ea130ecb62f6ef16acc8";
 var lat;
 var long;
 
-var getCurrentWeather = function (user) {
+var pastSearches = [];
+
+var getCurrentWeather = function () {
     var city = searchField.value.trim();
     currentContainer.innerHTML = "";
     // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
-    console.log(apiUrl)
   
     fetch(apiUrl)
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
+            var cityName = data.name;
+            var cityNameEl = document.createElement('h3')
+            cityNameEl.textContent = cityName;
+
+            if (!pastSearches.includes(cityName)) {
+                pastSearches.push(cityName);
+                
+                var historyBtn = document.createElement('button');
+                historyBtn.setAttribute('class', 'btn btn-secondary')
+                historyBtn.textContent = cityName;
+                searchHistoryContainer.append(historyBtn);
+              }
+            localStorage.setItem("pastSearches", JSON.stringify(pastSearches));
+            
             var iconcode = data.weather[0].icon;
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
-            var cityNameEl = document.createElement('h3')
-            cityNameEl.textContent = data.name;
             var iconEl = document.createElement('img')
             iconEl.setAttribute('src', iconurl);
+            
             cityNameEl.appendChild(iconEl);
             currentContainer.append(cityNameEl);
 
@@ -40,7 +54,6 @@ var getCurrentWeather = function (user) {
             humidityEl.textContent = "Humidity: " + data.main.humidity + " %"
             currentContainer.append(humidityEl);
 
-            console.log(data)
           });
         } else {
           alert('Error: ' + response.statusText);
