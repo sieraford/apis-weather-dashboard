4a334859312e3e6cc30c872a07b887d03aecdc50
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-11-21 15:55:02 -0800

    add comments to files

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var APIKey = "f31f098d0ca4ea130ecb62f6ef16acc8";
 
 var pastSearches = [];
 
-// The following function renders items in a todo list as <li> elements
+// The following function renders past search in a list as <button> elements
 function renderpastSearches() {
     for (var i = 0; i < pastSearches.length; i++) {
   
@@ -34,6 +34,7 @@ function init() {
     renderpastSearches();
 }
 
+// Fetches current weather from Open Weather API an displays it on the page
 var getCurrentWeather = function () {
     var city = searchField.value.trim();
     var cityNoSpaces = city.replace(" ", "+")
@@ -51,6 +52,7 @@ var getCurrentWeather = function () {
             var cityNameEl = document.createElement('h3');
             cityNameEl.textContent = cityName + ` (${date})`;
 
+            // If the user enters a city that doesn't already exist in the search history, then add it
             if (!pastSearches.includes(cityName)) {
                 pastSearches.push(cityName);
                 
@@ -61,6 +63,7 @@ var getCurrentWeather = function () {
               }
             localStorage.setItem("pastSearches", JSON.stringify(pastSearches));
             
+            // Get weather icon and display it next to city name
             var iconcode = data.weather[0].icon;
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
             var iconEl = document.createElement('img')
@@ -68,7 +71,8 @@ var getCurrentWeather = function () {
             
             cityNameEl.appendChild(iconEl);
             currentContainer.append(cityNameEl);
-
+            
+            // Get temp, wind and humidity data and display it in current weather section
             var tempEl = document.createElement('p');
             tempEl.textContent = "Temp: " + data.main.temp + "°F";
             currentContainer.append(tempEl);
@@ -91,6 +95,7 @@ var getCurrentWeather = function () {
       });
   };
 
+// Fetches 5 day weather forecast from Open Weather API an displays it on the page
 var getWeatherForecast = function (user) {
     var city = searchField.value.trim();
     var cityNoSpaces = city.replace(" ", "+")
@@ -107,10 +112,11 @@ var getWeatherForecast = function (user) {
             forecastHeaderText.textContent = "5-Day Forecast:";
             forecastContainer.append(forecastHeaderText);
 
-
+            // Function that gets date, temp, wind, humidity data and displays it on page
             function renderForecast(id, index) {
                 var dayCard = document.getElementById(id)
                 dayCard.innerHTML = "";
+                
                 var dateText = data.list[index].dt_txt;
                 var dateSliced = dateText.slice(0, 10);
                 var date = dayjs(dateSliced).format('M/D/YYYY');
@@ -137,6 +143,7 @@ var getWeatherForecast = function (user) {
                 dayCard.append(humidityEl);
             }
             
+            // Gets forecast data for the next 5 days from Open Weather API
             renderForecast("day-one", 0);
             renderForecast("day-two", 8);
             renderForecast("day-three", 16);
@@ -153,16 +160,17 @@ var getWeatherForecast = function (user) {
       });
   };
 
-  function getWeatherFromHistory(event) {
-    var element = event.target;
-    if(element.matches('.btn-secondary')){
-        searchField.value = element.textContent;   
-        getCurrentWeather();
-        getWeatherForecast();
-    }
+// If a user clicks a city from the search history, get current and future condiitions for that city  
+function getWeatherFromHistory(event) {
+  var element = event.target;
+  if(element.matches('.btn-secondary')){
+      searchField.value = element.textContent;   
+      getCurrentWeather();
+      getWeatherForecast();
   }
+}
 
-  searchButton.addEventListener('click', getCurrentWeather);
-  searchButton.addEventListener('click', getWeatherForecast);
-  document.addEventListener('click', getWeatherFromHistory);
-  init();
+searchButton.addEventListener('click', getCurrentWeather);
+searchButton.addEventListener('click', getWeatherForecast);
+document.addEventListener('click', getWeatherFromHistory);
+init();
